ee1513f7276c507ac05ee901a3af31883b809369
no <no>
2020-10-22 19:20:33 +0300

    init

---
+++ src/Barker.ts
@@ -0,0 +1,36 @@
+import { wrapService } from '@typeswarm/cli';
+import { StrictSpecification } from '@typeswarm/cli/lib/normalize';
+import { setImageTag } from '@typeswarm/utils';
+export interface BarkerOptions {
+    image?: string;
+    tag: string;
+
+    dbUser: string;
+    dbPassword: string;
+    dbHost: string;
+    dbPort?: number;
+    dbName: string;
+
+    serviceName?: string;
+}
+export const Barker: (options: BarkerOptions) => StrictSpecification = ({
+    dbHost,
+    dbName,
+    dbPassword,
+    dbUser,
+    tag,
+    dbPort = 3306,
+    image = 'docker.pkg.github.com/corporateanon/barker/barker',
+    serviceName = 'barker',
+}) => {
+    const service = wrapService({
+        environment: {
+            DB_DRIVER: 'mysql',
+            DB_CONNECTION: `${dbUser}:${dbPassword}@tcp(${dbHost}:${dbPort})/${dbName}?charset=utf8mb4&parseTime=True&loc=Local`,
+        },
+    }).with(setImageTag({ image, tag }));
+
+    return {
+        [serviceName]: service,
+    };
+};
